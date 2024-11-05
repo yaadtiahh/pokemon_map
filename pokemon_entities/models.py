c90b5bb2e400e509d9fdb17d19c8e9b5bcd31dd5
Lleanin <Fifashf@yandex.ru>
2024-11-05 18:00:40 +0100

    Make the fields optional

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,12 +3,12 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     """Покемон."""
-    title = models.CharField(max_length=200, blank=True, verbose_name='Название на рус.')
+    title = models.CharField(max_length=200, verbose_name='Название на рус.')
     title_en = models.CharField(max_length=200, blank=True, verbose_name='Название на англ.')
     title_jp = models.CharField(max_length=200, blank=True, verbose_name='Название на яп.')
 
     description = models.TextField(blank=True, verbose_name='Описание')
-    photo = models.ImageField(upload_to='pokemons_imgs', null=True, blank=True, verbose_name='Изображение')
+    photo = models.ImageField(upload_to='pokemons_imgs', null=True, verbose_name='Изображение')
 
     previous_evolution = models.ForeignKey(
         "self",
@@ -28,14 +28,14 @@ class PokemonEntity(models.Model):
     lat = models.FloatField(null=True, verbose_name='Широта')
     lon = models.FloatField(null=True, verbose_name='Долгота')
 
-    appeared_at = models.DateTimeField(null=True, verbose_name='Время начала')
-    disappeared_at = models.DateTimeField(null=True, verbose_name='Время окончания')
+    appeared_at = models.DateTimeField(null=True, blank=True, verbose_name='Время начала')
+    disappeared_at = models.DateTimeField(null=True, blank=True, verbose_name='Время окончания')
 
-    level = models.IntegerField(null=True, verbose_name='Уровень')
-    health = models.IntegerField(null=True, verbose_name='Здоровье')
-    atack = models.IntegerField(null=True, verbose_name='Урон')
-    defence = models.IntegerField(null=True, verbose_name='Броня')
-    endurance = models.IntegerField(null=True, verbose_name='Выносливость')
+    level = models.IntegerField(null=True, blank=True, verbose_name='Уровень')
+    health = models.IntegerField(null=True, blank=True, verbose_name='Здоровье')
+    atack = models.IntegerField(null=True, blank=True, verbose_name='Урон')
+    defence = models.IntegerField(null=True, blank=True, verbose_name='Броня')
+    endurance = models.IntegerField(null=True, blank=True, verbose_name='Выносливость')
 
     pokemon = models.ForeignKey(
         Pokemon,
